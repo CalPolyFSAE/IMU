391e536ce89e40e2cab214041b5182e011250b10
salticids <graydon@seewasp.com>
2019-05-15 09:54:26 -0700

    skeleton

---
+++ source/main.cpp
@@ -0,0 +1,76 @@
+/* System */
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MKE18F16.h"
+#include "fsl_debug_console.h"
+
+/* BSP */
+#include "System.h"
+#include "gpio.h"
+#include "spi.h"
+
+/* Project */
+
+#define SPIBUS 0
+#define SPICSPORT gpio::PortA
+#define SPICSPIN 0
+#define SPIBAUD 500000UL
+
+#define PINRESET gpio::PortA,0
+#define PINBOOTN gpio::PortA,0
+#define PINPS0 gpio::PortA,0
+#define PINPS1 gpio::PortA,0
+#define PINCLKSEL0 gpio::PortA,0
+#define PINCLKSEL1 gpio::PortA,0
+#define PINH_INTN gpio::PortA,0
+
+using namespace BSP;
+
+void spicallback(){
+
+}
+
+
+int main(void) {
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    //SysTick_Config(SYSTICK);
+    
+    // setup
+    gpio::GPIO::ConstructStatic();
+    gpio::GPIO& gpio = gpio::GPIO::StaticClass();
+
+    gpio.clear(PINRESET);
+    gpio.set(PINBOOTN);
+    gpio.set(PINPS0);
+    gpio.set(PINPS1);
+    gpio.set(PINCLKSEL0);
+    gpio.clear(PINCLKSEL1);
+    gpio.in_dir(PINH_INTN);
+    
+    spi::spi_config spiconf;
+    spiconf.callbacks[SPIBUS] = spicallback;
+    spi::SPI::ConstructStatic(&spiconf);
+    spi::SPI& spi = spi::SPI::StaticClass();
+
+    spi::SPI::masterConfig mconf;
+    mconf.baudRate = SPIBAUD;
+    mconf.cpol = kLPSPI_ClockPolarityActiveLow;
+    mconf.cphase = kLPSPI_ClockPhaseSecondEdge;
+    mconf.csport = SPICSPORT;
+    mconf.cspin = SPICSPIN;
+    spi.initMaster(SPIBUS, &mconf);
+
+    gpio.set(PINRESET);
+
+    while(gpio.read(PINH_INTN);
+
+    while(1){
+    }
+}
+
+extern "C" {
+void SysTick_Handler(void){
+
+}
+}
